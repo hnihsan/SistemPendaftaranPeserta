af21a7ac50138dbff01def9ac2766abaad222c88
PurwoSA <purwo.setyo.aji@gmail.com>
2017-08-08 16:03:28 +0700

    Menghapus yang tidak perlu dan menambah yang perlu

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="resources/js/jquery.js"></script>
   <script src="resources/semantic/semantic.min.js"></script>
   <script type="text/javascript" charset="utf8" src="resources/js/jquery.dataTables.min.js"></script>
   <script type="text/javascript" charset="utf8" src="resources/js/dataTables.semanticui.min.js"></script>
@@ -6,16 +6,11 @@
   $(document).ready( function () {
     $('#table1').DataTable({
       language: {
-        url: '//cdn.datatables.net/plug-ins/1.10.15/i18n/Indonesian.json'
+        url: 'resources/Indonesian.json'
       }
     });
     $('.ui.dropdown').dropdown();
     $('.ui.modal').modal('attach events', '.tambah.button', 'show');
-    $('.ui.search').search({
-      source:         content,
-      searchFields:   ['title'],
-      searchFullText: false
-    });
   } );
   </script>
 </body>
--- jurusan.sql
+++
@@ -1,4 +0,0 @@
-select fakultas.nama_fakultas, program_studi.program_studi
-from fakultas
-inner join program_studi
-on fakultas.kode_fakultas=program_studi.kode_fakultas
