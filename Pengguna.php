390250c306ce8d0087c46c940498a378abd14e73
PurwoSA <purwo.setyo.aji@gmail.com>
2017-08-19 15:45:34 +0700

    Menambahkan form ubah pengguna

--- Pengguna.php
+++ Pengguna.php
@@ -88,6 +88,44 @@ date_default_timezone_set("Asia/Jakarta");
           <input class="ui primary right button" type="submit" name="simpan" value="Simpan">
         </div>
       </div>
+      <div class="ui modal" id="ubah">
+        <i class="close icon"></i>
+        <div class="header">
+          Ubah Pengguna
+        </div>
+        <div class="content">
+          <form class="ui form" action="Pengguna.php" method="post">
+            <div class="field">
+              <label for="username">Nama Pengguna</label>
+              <input id="username" type="text" name="username" required="" maxlength="50" onkeypress="return (event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 96 && event.charCode <= 122) || (event.charCode >= 32 && event.charCode <= 32)">
+            </div>
+            <div class="two fields">
+              <div class="field">
+                <label for="password">Kata Sandi</label>
+                <input id="password" type="password" name="password" required="" maxlength="32">
+              </div>
+              <div class="field">
+                <label for="valid_pass">Konfirmasi Kata Sandi</label>
+                <input id="valid_pass" type="password" name="valid_pass" required="" maxlength="32">
+              </div>
+            </div>
+            <div class="two fields">
+              <div class="field">
+                <label for="phone">Telepon</label>
+                <input id="phone" type="text" name="phone" required="" maxlength="17" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+              </div>
+              <div class="field">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" required="" maxlength="100">
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="actions">
+          <input class="ui button" type="reset" value="Bersihkan">
+          <input class="ui primary right button" type="submit" name="update" value="Perbarui">
+        </div>
+      </div>
     </div>
   </div>
 <?php include 'footer.php'; ?>
